740bee814f1d1d7d85e3c0f07b6ea2d265662c41
Roman <65417719+RomanBaukin@users.noreply.github.com>
2020-08-07 14:27:13 +0400

    change

--- js/main.js
+++ js/main.js
@@ -1,15 +1,11 @@
-var instance = new vidbg(
-  ".video",
-  {
-    mp4: "video/world.mp4", // URL or relative path to MP4 video
-    webm: "video/world.webm", // URL or relative path to webm video
-    poster: "video/poster.jpg", // URL or relative path to fallback image
-    overlay: false, // Boolean to display the overlay or not
-  },
-  {
-    // Attributes
-  }
-);
+var instance = new vidbg(".video", {
+  mp4: "video/world.mp4", // URL or relative path to MP4 video
+  webm: "video/world.webm", // URL or relative path to webm video
+  poster: "video/poster.jpg", // URL or relative path to fallback image
+  overlay: false, // Boolean to display the overlay or not
+  overlayColor: "#000",
+  overlayAlpha: 0.3,
+});
 
 var rellax = new Rellax(".rocket");
 if (document.body.clientWidth < 576) {
